82edb44b9e187a7843b9851fd7d98d2919829f0b
Noé Diette <noe@diette.fr>
2024-09-18 11:01:04 +0200

    Création classe voiture

---
+++ src/voiture.py
@@ -0,0 +1,21 @@
+class Voiture:
+    """Classe représentant une voiture.
+
+    Attributes
+    ----------
+    nom : str
+        le nom de la voiture.
+    couleur : str
+        la couleur de la voiture.
+    vitesse : int
+        la vitesse de la voiture (initalisée à 0).
+    """
+
+    def __init__(self, nom, couleur):
+        """Constructeur"""
+        self.nom = nom
+        self.couleur = couleur
+        self.vitesse = 0
+
+    def __str__(self):
+      return f"La voiture {self.nom} de couleur {self.couleur} roule à {self.vitesse} km/h."
